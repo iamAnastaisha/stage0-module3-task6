5db6f32276d87355f3d0e8252b4102f84b39b7bf
nasty <anastaishastil2002@gmail.com>
2022-09-03 22:54:11 +0300

    Updated

--- src/main/java/lang/print/gaps/task6/OperatorPrecedence.java
+++ src/main/java/lang/print/gaps/task6/OperatorPrecedence.java
@@ -8,11 +8,11 @@ public class OperatorPrecedence {
         System.out.println(eight);//eight is expected to be 8
         boolean firstBool = false;
         boolean secondBool = false;
-        boolean thirdBool = 1 == 1;
-        boolean fourthBool = 4 <= 101;
+        boolean thirdBool = (1 == 1);
+        boolean fourthBool = (4 <= 101);
         int varSix = 17;
 
         System.out.println(firstBool && (secondBool || thirdBool && fourthBool || (varSix = 5) < 0));
-        System.out.println(varSix);
+        System.out.println(varSix = 5);
     }
 }
